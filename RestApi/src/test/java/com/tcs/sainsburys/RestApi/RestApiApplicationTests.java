68e1d7363bb40ca81d40a40e3627aca260e005e4
svvidyaraj <72314256+svvidyaraj@users.noreply.github.com>
2021-11-30 11:55:07 +0000

    Rest Api Demo
    Rest Api Demo using amazon jdk

---
+++ RestApi/src/test/java/com/tcs/sainsburys/RestApi/RestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcs.sainsburys.RestApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
